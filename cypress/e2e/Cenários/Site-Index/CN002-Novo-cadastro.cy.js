7f9925f8a23af29af6734e3d73c73973f0cf2301
Igor Lima <igorlimamp1@gmail.com>
2024-01-30 17:37:14 -0300

    terceiro dia - CN002

--- cypress/e2e/Cenários/Site-Index/CN002-Novo-cadastro.cy.js
+++ cypress/e2e/Cenários/Site-Index/CN002-Novo-cadastro.cy.js
@@ -1,7 +1,9 @@
-/// <reference types="Cypress" />
+import {faker} from '@faker-js/faker/locale/en'
 
 describe ('CN002', () => {
     it('Novo cadastro', () => {
+        const email = `${faker.datatype.uuid()}@gamail.com`
+
         cy.visit('https://demo.automationtesting.in/Index.html')
 
         cy.get('#enterimg')
@@ -15,6 +17,17 @@ describe ('CN002', () => {
 
         cy.get('textarea[rows="3"][ng-model="Adress"]')
         .type('Rua Professora Nina Stocco')
+
+        cy.get('input[type="email"]')
+        .type(email)
+
+        cy.get('input[type="tel"]')
+        .type('11965443322')
+
+        cy.get('input[type="radio"][value="Male"]')
+        .check()    
+
+
 
     })
 })
